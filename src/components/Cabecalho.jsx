0bca70f081880636b8a0d22913a2eb7b39d83e11
DiegoFZL <diego.amorim@aluno.cps.sp.gov.br>
2026-08-19 10:02:12 -0300

    created first react components

---
+++ src/components/Cabecalho.jsx
@@ -0,0 +1,10 @@
+export default function Cabecalho() {
+    return (
+        <header className="cabecalho">
+            <div className="cabecalho-conteudo">
+                <h1>Sistema de Gestão de Comércio</h1>
+                <p>Gerenciamento de produtos, clientes e funcionários</p>
+            </div>
+        </header>
+    )
+}
---
+++ src/components/CardModulo.jsx
@@ -0,0 +1,8 @@
+export default function CardModulo({titulo, descricao}) {
+	return (
+		<article className="card-modulo">
+			<h2>{titulo}</h2>
+			<p>{descricao}</p>
+		</article>
+	)
+}
